cc91aedad43ac14ff6d5fec1f416e76e379861dd
chidoriashi1990 <chidoriashi00918@gmail.com>
2018-05-27 19:48:06 +0900

    Add Toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,50 @@
 <template>
-  <div id="app">
-    <v-app>
-      <!-- ナビゲーション -->
-      <v-navigation-drawer v-model="drawer" fixed app>
-        <v-toolbar flat>
-          <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
-          <v-toolbar-title>Aplication</v-toolbar-title>
-        </v-toolbar>
-        <v-divider></v-divider>
-        <v-list dense class="pt-0">
-          <v-list-tile v-for="item in items" :key="item.title">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
-      <!-- ツールバー -->
-      <v-toolbar app>
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
-        <v-toolbar-title>What is Atomic Design</v-toolbar-title>
-      </v-toolbar>
-      <!-- コンテンツ -->
-      <v-content>
-        <v-container fluid>
-          <transition>
-          <router-view/>
-          </transition>
-        </v-container>
-      </v-content>
-      <!-- フッター -->
-      <v-footer app>
-      </v-footer>
-    </v-app>
-  </div>
+  <v-app>
+    <!-- ツールバー・ナビゲーション -->
+    <!-- ナビゲーション -->
+    <v-navigation-drawer v-model="drawer" fixed clipped app>
+      <!-- リンク -->
+      <list/>
+      <!-- ユーザーグループ -->
+      <list-group/>
+    </v-navigation-drawer>
+    <!-- ツールバー -->
+    <v-toolbar color="blue darken-3" clipped-left app>
+      <v-toolbar-side-icon class="white--text" @click.stop="drawer = !drawer"/>
+      <v-toolbar-title class="white--text">What is Atomic Design</v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon>
+        <v-icon class="white--text">more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <!-- コンテンツ -->
+    <v-content>
+      <v-container app>
+        <transition>
+        <router-view/>
+        </transition>
+      </v-container>
+    </v-content>
+    <!-- フッター -->
+    <v-footer color="blue darken-3" app>
+      <v-toolbar-title class="white--text">What is Atomic Design</v-toolbar-title>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import List from '@/components/120.molecules/List'
+import ListGroup from '@/components/120.molecules/ListGroup'
 
 export default {
   name: 'App',
   components: {
-
+    List,
+    ListGroup
   },
   data: () => ({
-    drawer: null,
-    items: [
-      { title: 'Home', icon: 'dashboard' },
-      { title: 'About', icon: 'question_answer' }
-    ]
+    drawer: false
   })
 }
 </script>
---
+++ src/components/120.molecules/ListGroup.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-list>
+    <v-list-group v-for="subItem in subItems" :key="subItem.title" prepend-icon="account_circle" value="true">
+      <v-list-tile slot="activator">
+        <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+      </v-list-tile>
+      <v-list-tile v-for="item in subItem.items" :key="item.title">
+        <v-list-tile-action><v-icon/></v-list-tile-action>
+        <v-list-tile-content><v-list-tile-title>{{ item.title }}</v-list-tile-title></v-list-tile-content>
+      </v-list-tile>
+    </v-list-group>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: location.href.split(/\/(?!.*\/)/)[1].split(/\.(?!.*\.)/)[0],
+  data () {
+    return {
+      subItems: [
+        {
+          title: 'AdminUser',
+          items: [
+            { title: 'user1' },
+            { title: 'user2' },
+            { title: 'user3' }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// $cardHeight: 0vh;
+</style>
