19eee5435e9a9f2f13b7006a85c8b508a73c26bb
chidoriashi1990 <chidoriashi00918@gmail.com>
2018-05-27 11:34:34 +0900

    pull Vuetify css

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
-    <div class="main-margin-top">
+    <v-app>
+      <v-navigation-drawer app></v-navigation-drawer>
+      <v-toolbar app>
+        <v-toolbar-side-icon/>
+      </v-toolbar>
+      <v-content>
+        <v-container fluid>
       <transition>
       <router-view/>
       </transition>
-    </div>
+        </v-container>
+      </v-content>
+      <v-footer app></v-footer>
+    </v-app>
   </div>
 </template>
 
 <script>
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
 export default {
   name: 'App',
   components: {
@@ -18,11 +29,14 @@ export default {
 </script>
 
 <style lang="scss">
+@import '../node_modules/vuetify/dist/vuetify.min.css';
+
 #app {
   // 横スクロールを禁止
   overflow-x: hidden;
 }
 a {
+  // リンクの下線を消す
   text-decoration: none;
 }
 </style>
--- src/components/150.pages/Top.vue
+++ src/components/150.pages/Top.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="Empty">
+  <div id="Top">
   </div>
 </template>
 
@@ -13,7 +13,6 @@ export default {
   },
   data () {
     return {
-      msg: 'Empty',
       source: []
     }
   },
@@ -25,72 +24,6 @@ export default {
     tmp (val) {
 
     }
-  },
-  props: {
-    prop1: {
-      type: String,
-      default: 'default',
-      required: true // requiredは必ず要求するプロパティ
-    }
-  },
-  computed: {
-
-  },
-  /**
-   * インスタンスが初期化されるときに同期的に呼ばれる
-   */
-  beforeCreate () {
-
-  },
-  /**
-   * インスタンスが作成された後に同期的に呼ばれる
-   */
-  created () {
-
-  },
-  /**
-   * インスタンスがマウントされる前に呼ばれる
-   * templateオプションがrender関数にコンパイルされた後に実行される
-   * （templateオプションが無い場合は、elで指定したouterHTMLをコンパイルする）
-   */
-  beforeMount () {
-
-  },
-  /**
-   * インスタンスがマウントされた後に呼ばれる
-   * DOM要素にアクセスできるようになる（this.$el）
-   */
-  mounted () {
-
-  },
-  /**
-   * 状態を更新し、Virtual DOMが再描画される前に呼ばれる
-   * つまり、beforeUpdate内で状態を取得すると更新後の値になっている
-   */
-  beforeUpdate () {
-
-  },
-  /**
-   * 状態を更新し、Virtual DOMが再描画された後に呼ばれる
-   * 状態変更後のDOM要素にアクセスする場合は、updated内で取得すると良い
-   * ただし、updated内で状態を更新すると無限ループに陥る可能性がある
-   * （状態を更新してループを抜ける条件をちゃんと実装しておく必要がある）
-   */
-  updated () {
-
-  },
-  /**
-   * インスタンスが破棄される前に呼ばれる
-   * この段階ではインスタンスはまだ完全に機能している
-   */
-  beforeDestroy () {
-
-  },
-  /**
-   * インスタンスが破棄された後に呼ばれる
-   */
-  destroyed () {
-
   }
 }
 </script>
