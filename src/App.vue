3c4fc262fa15b06e6c8f068505e02e0a4035d081
chidoriashi1990 <chidoriashi00918@gmail.com>
2018-05-27 14:13:26 +0900

    create sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,40 @@
 <template>
   <div id="app">
     <v-app>
-      <v-navigation-drawer app></v-navigation-drawer>
+      <!-- ナビゲーション -->
+      <v-navigation-drawer v-model="drawer" fixed app>
+        <v-toolbar flat>
+          <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
+          <v-toolbar-title>Aplication</v-toolbar-title>
+        </v-toolbar>
+        <v-divider></v-divider>
+        <v-list dense class="pt-0">
+          <v-list-tile v-for="item in items" :key="item.title">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <!-- ツールバー -->
       <v-toolbar app>
-        <v-toolbar-side-icon/>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"/>
+        <v-toolbar-title>What is Atomic Design</v-toolbar-title>
       </v-toolbar>
+      <!-- コンテンツ -->
       <v-content>
         <v-container fluid>
-      <transition>
-      <router-view/>
-      </transition>
+          <transition>
+          <router-view/>
+          </transition>
         </v-container>
       </v-content>
-      <v-footer app></v-footer>
+      <!-- フッター -->
+      <v-footer app>
+      </v-footer>
     </v-app>
   </div>
 </template>
@@ -24,7 +46,14 @@ export default {
   name: 'App',
   components: {
 
-  }
+  },
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: 'Home', icon: 'dashboard' },
+      { title: 'About', icon: 'question_answer' }
+    ]
+  })
 }
 </script>
 
