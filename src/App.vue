2fdb5b58814f77eb9331d1fbf56c1032167f2438
chidoriashi1990 <chidoriashi00918@gmail.com>
2018-06-04 20:34:18 +0900

    Combine into component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,24 @@
 <template>
   <v-app>
-    <!-- ツールバー -->
-    <v-toolbar clipped-left app dark height="60">
-      <v-toolbar-title class="white--text">What is Atomic Design</v-toolbar-title>
-      <v-spacer/>
-      <v-btn icon>
-        <v-icon class="white--text">more_vert</v-icon>
-      </v-btn>
-    </v-toolbar>
+    <my-toolbar/>
     <!-- コンテンツ -->
-    <v-content>
-      <v-container app>
-        <transition>
-        <router-view/>
-        </transition>
-      </v-container>
-    </v-content>
-    <!-- フッター -->
-    <v-footer height="50" app dark>
-      <v-toolbar-title></v-toolbar-title>
-    </v-footer>
+    <transition>
+    <router-view/>
+    </transition>
+    <my-footer/>
   </v-app>
 </template>
 
 <script>
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import List from '@/components/120.molecules/List'
-import ListGroup from '@/components/120.molecules/ListGroup'
+import MyToolbar from '@/components/130.organisms/Toolbar'
+import MyFooter from '@/components/130.organisms/Footer'
 
 export default {
   name: 'App',
   components: {
-    List,
-    ListGroup
+    MyToolbar,
+    MyFooter
   },
   data: () => ({
     drawer: false
