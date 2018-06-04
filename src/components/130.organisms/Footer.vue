3a6f07bb258a593b5098607b35b5210ec5bcac33
chidoriashi1990 <chidoriashi00918@gmail.com>
2018-06-04 20:34:48 +0900

    create footer

---
+++ src/components/130.organisms/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- フッター -->
+  <v-footer height="50" app fixed dark>
+    <v-flex xs12 py-3 text-xs-center>
+      &copy;{{ new Date().getFullYear() }} — <strong>Yusuke Miyakawa</strong>
+    </v-flex>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: location.href.split(/\/(?!.*\/)/)[1].split(/\.(?!.*\.)/)[0]
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
