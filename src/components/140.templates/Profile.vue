0730186dba094d8872b7a7ea55ca7f08135f6115
chidoriashi1990 <chidoriashi00918@gmail.com>
2018-06-04 20:35:43 +0900

    add profile component

---
+++ src/components/140.templates/Profile.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="Profile">
+    <v-jumbotron>
+      <v-container fill-height>
+        <v-layout align-center>
+          <v-flex text-xs-center>
+            <h3 class="display-3">Gradient over Image</h3>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-jumbotron>
+
+    <v-content>
+      <v-container app>
+        <img src="./../../assets/profile/g4712.png" alt="">
+      </v-container>
+    </v-content>
+
+    <!-- Return top -->
+    <v-fab-transition>
+      <v-btn dark color="indigo" fab fixed bottom right
+             @click="$vuetify.goTo('#top', option)">
+        <v-icon>keyboard_arrow_up</v-icon>
+      </v-btn>
+    </v-fab-transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: location.href.split(/\/(?!.*\/)/)[1].split(/\.(?!.*\.)/)[0],
+  components: {
+
+  },
+  data () {
+    return {
+      option: {
+        duration: 800
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/150.pages/Top.vue
+++ src/components/150.pages/Top.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="display-1">Top</div>
+  <div id="top">
+    <profile/>
+  </div>
 </template>
 
 <script>
+import Profile from '@/components/140.templates/Profile'
+
 export default {
   name: location.href.split(/\/(?!.*\/)/)[1].split(/\.(?!.*\.)/)[0],
   components: {
-
+    Profile
   }
 }
 </script>
