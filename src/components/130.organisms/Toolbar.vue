b8bb8efb280036e4eca6ea32fafb8fbbfab4ff6b
chidoriashi1990 <chidoriashi00918@gmail.com>
2018-06-04 20:34:38 +0900

    create toolbar

---
+++ src/components/130.organisms/Toolbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- ツールバー -->
+  <v-toolbar clipped-left app dark height="60">
+    <v-toolbar-title class="white--text">
+      github.io
+    </v-toolbar-title>
+    <!-- リンクボタン -->
+    <v-toolbar-items v-for="item in linkItems" v-bind:key="item.label">
+      <v-btn flat @click="$vuetify.goTo(item.to, option)">{{ item.label }}</v-btn>
+    </v-toolbar-items>
+    <v-spacer/>
+    <v-tooltip left>
+      <v-btn icon class="white" slot="activator" href="https://github.com/chidoriashi1990" target="_blank">
+        <img src="./../../assets/github/GitHub-Mark-32px.png">
+      </v-btn>
+      <span>Go to GitHub</span>
+    </v-tooltip>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: location.href.split(/\/(?!.*\/)/)[1].split(/\.(?!.*\.)/)[0],
+  data: () => ({
+    linkItems: [
+      { icon: '', label: 'HOME', to: '#top' },
+      { icon: '', label: 'Content 1', to: '#top' },
+      { icon: '', label: 'Content 2', to: '#top' }
+    ]
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
