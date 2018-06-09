501a9ec7a89d1898691184d2d132f0f3e198398a
chidoriashi1990 <chidoriashi00918@gmail.com>
2018-06-09 16:19:11 +0900

    remove toolbar and footer

--- src/components/150.pages/Top.vue
+++ src/components/150.pages/Top.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="top">
+    <my-toolbar/>
     <profile/>
+    <my-footer/>
   </div>
 </template>
 
 <script>
+import MyToolbar from '@/components/130.organisms/Toolbar'
 import Profile from '@/components/140.templates/Profile'
+import MyFooter from '@/components/130.organisms/Footer'
 
 export default {
   name: location.href.split(/\/(?!.*\/)/)[1].split(/\.(?!.*\.)/)[0],
   components: {
-    Profile
+    MyToolbar,
+    Profile,
+    MyFooter
   }
 }
 </script>
