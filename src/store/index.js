bb3c392997e8cc56db35e94989e124f5c016786e
Yusuke Miyakawa <chidoriashi00918@gmail.com>
2018-07-29 14:22:53 +0900

    ProfileのdataをVuexモジュール化

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,13 @@ import Vuex from 'vuex'
 
 // modules
 import moduleUserInfo from '@/store/modules/userInfo'
+import moduleProfile from '@/store/modules/profile'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    userInfo: moduleUserInfo
+    userInfo: moduleUserInfo,
+    profile: moduleProfile
   }
 })
