a7bdd633d5994f486b56abca77c6e9a9ff7bb8de
Yusuke Miyakawa <chidoriashi00918@gmail.com>
2019-01-14 17:16:35 +0900

    Del Sample vue component

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,11 @@
 <template>
   <v-app>
-    <app-logo/>
   </v-app>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
   components: {
-    AppLogo
   }
 }
 </script>
